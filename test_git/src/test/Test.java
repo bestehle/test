e04b5c369eb09bd982d3bafaf13229eae8f980d1
bestehle <bestehle@htwg-konstanz.de>
2013-03-14 22:44:12 +0100

    test22

--- test_git/src/test/Test.java
+++ test_git/src/test/Test.java
@@ -7,7 +7,8 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hallo");
-		System.out.println("Hallo");
+		System.out.println("Halsdfsdffdlo");
+		System.out.println("Halsdfdsflo");
 	}
 
 }
