27bc0e353bc4d1511c09ccbd04909eec83c927f9
bestehle <bestehle@htwg-konstanz.de>
2013-03-26 15:15:48 +0100

    Update Test.java

--- test_git/src/test/Test.java
+++ test_git/src/test/Test.java
@@ -11,15 +11,19 @@ public class Test {
 		System.out.println("Halsdfsdffdlo");
 		System.out.println("Halsdfdsflo");
 
+		System.out.println("Halsdfsdffdlo");
+		System.out.println("Halsdfdsflo");	
+		System.out.println("Halsdfsdffdlo");
+		System.out.println("Halsdfdsflo");
+				System.out.println("Halsdfsdffdlo");
+		System.out.println("Halsdfdsflo");
+
 		System.out.println("Hallo");
 		System.out.println("Hallo von github");
 
 		System.out.println("Hallo");
 		System.out.println("Halsdfsdlo");
-		System.out.println("Halsdfsdflo");
-		System.out.println("Hallo vodsfn github");
-		System.out.println("HWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
-		System.out.println("Hallo");
+		System.out.println("Hald!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!llo");
 		System.out.println("Hallo von github");
 		System.out.println("Hallo von github");
 	}
