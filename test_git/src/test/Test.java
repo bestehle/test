499e99c47df640e4ef33b5da4fd3b51f19157182
bestehle <bestehle@htwg-konstanz.de>
2013-03-26 17:09:04 +0100

    master

--- test_git/src/test/Test.java
+++ test_git/src/test/Test.java
@@ -7,7 +7,10 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hallo");
-
+		System.out.println("Hallo");
+		
+		
+		System.out.println("Hallo");		System.out.println("Hallo");		System.out.println("Hallo");
 
 		System.out.println("Hallo");
 		System.out.println("Hallo von github");
